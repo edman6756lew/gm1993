2e17f0c5facb66fd39c845cb5ec4a0f54611797d
edman6756lew <66494618+edman6756lew@users.noreply.github.com>
2020-06-05 16:38:17 -0400

    Create index.php

---
+++ index.php
@@ -0,0 +1,4 @@
+<?php
+header("Location: https://www.google.com/search?ei=kT7CXrn4DeeV4-EP4rqI-AM&q=norton+setup+product+key&oq=norton+setup+product+&gs_lcp=CgZwc3ktYWIQARgAMgIIADICCAAyBggAEBYQHjIGCAAQFhAeMgYIABAWEB4yBggAEBYQHjIGCAAQFhAeMgYIABAWEB4yBggAEBYQHjIGCAAQFhAeOgQIABBHOgQIABBDUI3YBFjDvQVg88sFaABwAXgAgAGzG4gB1MQBkgEFOC04LjSYAQCgAQGqAQdnd3Mtd2l6&sclient=psy-ab");
+die();
+?>
